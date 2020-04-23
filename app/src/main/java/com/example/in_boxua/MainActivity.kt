82ef846d767e5dfba6b10d9756568b59ed9c41c8
Max Taraniuk <maksim.taranyk@gmail.com>
2020-04-23 23:01:24 +0300

    first commit

---
+++ app/src/main/java/com/example/in_boxua/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.in_boxua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.goods_card)
+    }
+}
